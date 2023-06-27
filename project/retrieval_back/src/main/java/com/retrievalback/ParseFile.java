17d4869c33cd1669ad67ec4065a8b51545263c86
DQQ233 <137767192+DQQ233@users.noreply.github.com>
2023-06-28 01:24:36 +0800

    Add files via upload

---
+++ project/retrieval_back/src/main/java/com/retrievalback/ParseFile.java
@@ -0,0 +1,58 @@
+package com.retrievalback.entity;
+
+import org.apache.ibatis.jdbc.Null;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.poi.POIXMLDocument;
+import org.apache.poi.POIXMLTextExtractor;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+/**
+ * @Author:
+ * @Data:2023/06/27
+ * @Description:解析word和pdf
+ */
+public class ParseFile {
+    /**
+     * 解析word和pdf，由于word有两个版本所以多了一个判断
+     * @param filePath
+     * @return
+     * @throws Exception
+     */
+    public  String FilePars(String filePath) throws Exception{
+        String text = null;
+        if(filePath.isEmpty()){
+            return "";
+        }
+        if(filePath.toLowerCase().endsWith("doc")){
+            InputStream file = new FileInputStream(new File(filePath));
+            WordExtractor wordExtractor = new WordExtractor(file);
+            text = wordExtractor.getText();
+            file.close();
+            wordExtractor.close();
+        }else if(filePath.toLowerCase().endsWith("docx")){
+            OPCPackage opcPackage = POIXMLDocument.openPackage(filePath);
+            POIXMLTextExtractor extractor = new XWPFWordExtractor(opcPackage);
+            text = extractor.getText();
+            opcPackage.close();
+            extractor.close();
+        }else if(filePath.toLowerCase().endsWith("pdf")){
+            PDDocument pdDocument;
+            InputStream file = new FileInputStream(new File(filePath));
+            pdDocument = PDDocument.load(file);
+            PDFTextStripper stripper = new PDFTextStripper();
+            text = stripper.getText(pdDocument);
+            file.close();
+            pdDocument.close();
+        }
+        return  text;
+    }
+
+
+
+}
